963b6fc6dcf12d0d6a73ae463dfa0dc92617643c
Uladzimir Hitsarau <vladimir.gitsarev@gmail.com>
2021-05-06 18:42:22 +0300

    update post view to create post

--- microblog_backend/blog/serializers.py
+++ microblog_backend/blog/serializers.py
@@ -4,13 +4,15 @@ from authentication.serializers import SmallUserSerializer
 from blog.models import Post, Comment
 
 
-class PostSerializer(serializers.ModelSerializer):
+class RetrievePostSerializer(serializers.ModelSerializer):
     user = SmallUserSerializer()
 
-    def create(self, validated_data):
-        user = self.context['request'].user
-        validated_data['user'] = user
-        return super().create(validated_data)
+    class Meta:
+        model = Post
+        fields = ('id', 'user', 'body', 'soft_deleted', 'created_at', 'updated_at', 'likes', 'dislikes', 'repost', )
+
+
+class CreatePostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
