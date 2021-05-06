6bc569777ae822254389108ea61395c1a84a554f
Uladzimir Hitsarau <vladimir.gitsarev@gmail.com>
2021-05-06 23:28:23 +0300

    correct post images uploading

--- microblog_backend/blog/models.py
+++ microblog_backend/blog/models.py
@@ -30,7 +30,7 @@ class Comment(CreatedAt, UpdatedAt, SoftDelete):
 class PostImage(CreatedAt):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    image = models.ImageField(blank=True, upload_to='posts/')
+    image = models.ImageField(blank=True, upload_to='posts/%Y/%m/%d')
 
     def __str__(self):
         return f'Image {self.image} for {self.post}'
--- microblog_backend/blog/serializers.py
+++ microblog_backend/blog/serializers.py
@@ -15,15 +15,6 @@ class RetrievePostSerializer(serializers.ModelSerializer):
 
 class CreatePostSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        if len(dict(self.context['request'].FILES)['images']) > 5:
-            raise ValidationError({'details': 'There cannot be more than 5 images for one post.'})
-        else:
-            post = super().create(validated_data)
-            for image in dict(self.context['request'].FILES)['images']:
-                post.postimage_set.create(image=image, user=self.context['request'].user)
-            return post
-
     class Meta:
         model = Post
         fields = ('id', 'user', 'body', 'soft_deleted', 'created_at', 'updated_at', 'likes', 'dislikes', 'repost', 'images')
